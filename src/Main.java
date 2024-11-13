dd5acec3760eca79edb62e9932440e625ab4b3ac
mss49 <124702052+mss49@users.noreply.github.com>
2024-11-13 16:53:41 +0000

    Add files via upload
    Created main and class files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * Starts the console UI for the MusicPlayer
+ */
+public class Main {
+	private static MusicPlayer musicplayer;
+
+
+}
+
