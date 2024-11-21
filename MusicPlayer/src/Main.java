84a53f41e096a52686c98cce428a306e05b9695f
dgh11BrumTown <dgh11@student.le.ac.uk>
2024-11-21 20:13:28 +0000

    Add files via upload

---
+++ MusicPlayer/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * Starts the console UI for the MusicPlayer
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+}
+
---
+++ MusicPlayer/src/model/Artist.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Artist {
+	private String name;
+	//implement in CSV text file with 
+	private int artist_id;
+	private ArrayList<Integer> song_ids;
+	
+	
+	
+	//default constructor
+	public Artist(String name, ArrayList<Integer> song_ids) {
+		super();
+		this.name = name;
+		this.song_ids = song_ids;
+	}
+	
+	//getter and setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Integer> getSong_ids() {
+		return song_ids;
+	}
+	public void setSong_ids(ArrayList<Integer> song_ids) {
+		this.song_ids = song_ids;
+	}
+	
+	
+}
