9aa29c74dc30d3ece054acc89f8fc1ef50b658ba
mss49 <124702052+mss49@users.noreply.github.com>
2024-11-27 13:40:37 +0000

    Add files via upload

--- model/Song.java
+++ model/Song.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.util.Date;
 import java.util.List;
@@ -13,18 +14,25 @@ public class Song {
 	private Date releaseDate;
 	private boolean isfavourite;
 	String mp3FilePath;
+	private int songId;
+	private FileInputStream fileInputStream;
+	private BufferedInputStream bufferedInputStream;
 	
 	
 	//returns a Player object which can be used by the controller to play a song
 	//returns an error or null pointer if filepath is incorrect
-	public Player getSongPlayer() {
-		try(FileInputStream fis = new FileInputStream(this.mp3FilePath)){
-			Player player = new Player(fis);
-			return player;
-		}catch(Exception e) {
-			e.printStackTrace();
+	public Player getSongPlayer() throws Exception {
+		if (fileInputStream != null) {
+			try {
+				fileInputStream.close();
+			} catch (Exception e) {
+				// Ignore closing errors
+			}
 		}
-		return null;
+		
+		fileInputStream = new FileInputStream(this.mp3FilePath);
+		bufferedInputStream = new BufferedInputStream(fileInputStream);
+		return new Player(bufferedInputStream);
 	}
 	
 	//must save the song to CSV
@@ -33,8 +41,9 @@ public class Song {
 	}
 
 	//constructor
-	public Song(String songTitle, List<Artist> artistList, Date releaseDate, boolean isfavourite, String mp3FilePath) {
+	public Song(int songId, String songTitle, List<Artist> artistList, Date releaseDate, boolean isfavourite, String mp3FilePath) {
 		super();
+		this.songId = songId;
 		this.songTitle = songTitle;
 		this.artistList = artistList;
 		this.releaseDate = releaseDate;
@@ -90,7 +99,34 @@ public class Song {
 	public void setMp3FilePath(String mp3FilePath) {
 		this.mp3FilePath = mp3FilePath;
 	}
+
+	public int getSongId() {
+		return songId;
+	}
+
+	public void setSongId(int songId) {
+		this.songId = songId;
+	}
+
+	@Override
+	public String toString() {
+		return songTitle;
+	}
 	
-	
+	public void closeStreams() {
+		try {
+			if (bufferedInputStream != null) {
+				bufferedInputStream.close();
+			}
+			if (fileInputStream != null) {
+				fileInputStream.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			bufferedInputStream = null;
+			fileInputStream = null;
+		}
+	}
 	
 }
